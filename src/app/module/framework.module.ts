dcba0855ee5b2ec16c6b3f0e86d78eecc779f46f
MassimoEsperto <em.decarlo@gmail.com>
2020-09-27 19:45:24 +0200

    Create framework.module.ts

---
+++ src/app/module/framework.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+const modules = []
+
+@NgModule({
+  imports: [modules],
+  exports: [modules],
+  declarations: []
+})
+export class AppCustomFrameworkModule { }
