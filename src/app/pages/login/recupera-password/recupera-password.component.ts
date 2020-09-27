4bc51ea81a8620cc95caf64dcb0e6a87a48bafba
MassimoEsperto <em.decarlo@gmail.com>
2020-09-27 19:48:01 +0200

    Create recupera-password.component.ts

---
+++ src/app/pages/login/recupera-password/recupera-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recupera-password',
+  templateUrl: './recupera-password.component.html',
+  styleUrls: ['./recupera-password.component.scss']
+})
+export class RecuperaPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
