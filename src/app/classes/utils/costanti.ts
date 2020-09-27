ca0352cfb3c9c67d2ab4ed3da079b4076f8a853f
MassimoEsperto <em.decarlo@gmail.com>
2020-09-27 19:44:56 +0200

    Create costanti.ts

---
+++ src/app/classes/utils/costanti.ts
@@ -0,0 +1,15 @@
+// COSTANTI
+
+// Versione applicazione
+export const VERSION = "1.0.2"
+
+export const WS_BASE_URL =  `http://marescafantaeuropeo.altervista.org/webServices`
+
+// Timeout richieste POST
+export const REQUEST_TIMEOUT = 2*60*1000 // 2 minuti
+
+// Tentativi richieste POST
+export const REQUEST_RETRIES = 3
+
+// Risposta di successo
+export const SUCCESS = "Incredibile Ha Funzionato!"
