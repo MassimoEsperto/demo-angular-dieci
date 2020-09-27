9c136b6c7a01f9f857ea84f75fefee9dc2ae95e1
MassimoEsperto <em.decarlo@gmail.com>
2020-09-27 19:45:17 +0200

    Create app-routing.module.ts

---
+++ src/app/module/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RecuperaPasswordComponent } from '../pages/login/recupera-password/recupera-password.component';
+import { RegisterComponent } from '../pages/login/register/register.component';
+import { SignInComponent } from '../pages/login/sign-in/sign-in.component';
+import { PresentationComponent } from '../pages/other/presentation/presentation.component';
+
+
+const routes: Routes = [
+  { 
+      path: '', 
+      redirectTo: '/login', 
+      pathMatch: 'full'
+  },
+  {
+      path: 'login',
+      component: PresentationComponent,
+      data: {animation: 'AboutPage'}
+  },
+  {
+      path: 'sign-in',
+      component: SignInComponent,
+      data: {animation: 'FilterPage'}
+  },
+  {
+      path: 'register',
+      component: RegisterComponent,
+      data: {animation: 'HomePage'}
+  },
+  {
+      path: 'recupera-password',
+      component: RecuperaPasswordComponent,
+      data: {animation: 'HomePage'}
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
