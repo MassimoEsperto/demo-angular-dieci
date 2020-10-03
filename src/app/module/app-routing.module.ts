73493da9b31208b245177095f452f1d378cd87f3
MassimoEsperto <em.decarlo@gmail.com>
2020-10-03 15:00:35 +0200

    Update app-routing.module.ts

--- src/app/module/app-routing.module.ts
+++ src/app/module/app-routing.module.ts
@@ -4,38 +4,44 @@ import { RecuperaPasswordComponent } from '../pages/login/recupera-password/recu
 import { RegisterComponent } from '../pages/login/register/register.component';
 import { SignInComponent } from '../pages/login/sign-in/sign-in.component';
 import { PresentationComponent } from '../pages/other/presentation/presentation.component';
+import { ShowCaseComponent } from '../pages/other/show-case/show-case.component';
 
 
 const routes: Routes = [
-  { 
-      path: '', 
-      redirectTo: '/login', 
-      pathMatch: 'full'
-  },
-  {
-      path: 'login',
-      component: PresentationComponent,
-      data: {animation: 'AboutPage'}
-  },
-  {
-      path: 'sign-in',
-      component: SignInComponent,
-      data: {animation: 'FilterPage'}
-  },
-  {
-      path: 'register',
-      component: RegisterComponent,
-      data: {animation: 'HomePage'}
-  },
-  {
-      path: 'recupera-password',
-      component: RecuperaPasswordComponent,
-      data: {animation: 'HomePage'}
-  },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: PresentationComponent,
+        data: { animation: 'AboutPage' }
+    },
+    {
+        path: 'sign-in',
+        component: SignInComponent,
+        data: { animation: 'FilterPage' }
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        data: { animation: 'HomePage' }
+    },
+    {
+        path: 'recupera-password',
+        component: RecuperaPasswordComponent,
+        data: { animation: 'HomePage' }
+    },
+    {
+        path: 'show-case',
+        component: ShowCaseComponent,
+        data: { animation: 'HomePage' }
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes,{useHash: true})],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
