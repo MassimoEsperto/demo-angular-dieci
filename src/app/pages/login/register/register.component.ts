20cb7db609cee270787f9a836f786c240b978aea
MassimoEsperto <em.decarlo@gmail.com>
2020-10-03 15:00:05 +0200

    Update register.component.ts

--- src/app/pages/login/register/register.component.ts
+++ src/app/pages/login/register/register.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { Utils } from 'src/app/classes/utils/utils';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef){
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
-
+  ngAfterViewInit(){
+    Utils.backgroundBlue(this.elementRef)
+ }
 }
